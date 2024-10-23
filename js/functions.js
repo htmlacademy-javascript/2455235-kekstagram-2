6d3334a3c742874ad25e2085aa8d8c55f40679fe
TatianaSenatorova <108460915+TatianaSenatorova@users.noreply.github.com>
2024-10-23 11:34:51 +0300

    fix: корректирует и оптимизирует код

--- js/functions.js
+++ js/functions.js
@@ -4,9 +4,9 @@ function checkStringLenght(string, stringLenght) {
   return string.length <= stringLenght;
 }
 
-console.log(checkStringLenght('проверяемая строка', 20));
-console.log(checkStringLenght('проверяемая строка', 18));
-console.log(checkStringLenght('проверяемая строка', 10));
+(checkStringLenght('проверяемая строка', 20));
+(checkStringLenght('проверяемая строка', 18));
+(checkStringLenght('проверяемая строка', 10));
 
 // Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
 
@@ -19,17 +19,14 @@ function checkPallindrome(string) {
   return false;
 }
 
-console.log(checkPallindrome('Лёша на полке клопа нашёл '));
-console.log(checkPallindrome('Миша на полке клопа нашёл '));
-console.log(checkPallindrome('Миша 3а полке /лопа нашёл '));
-console.log(checkPallindrome('3'));
-console.log(checkPallindrome('3*3'));
+(checkPallindrome('Лёша на полке клопа нашёл '));
+(checkPallindrome('Миша на полке клопа нашёл '));
 
 // Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:Если хотите усложнить задание, предусмотрите случай, когда вместо строки приходит число. Обратите внимание, что возвращать функция по-прежнему должна только целые положительные числа:
 
 const numbers = (data) => {
   if(typeof data === 'number') {
-    return data;
+    return Math.abs(data);
   }
 
   let resultNumber = '';
@@ -47,9 +44,8 @@ const numbers = (data) => {
   return NaN;
 };
 
-console.log(numbers('2023 год'));
-console.log(numbers('2год023 '));
-console.log(numbers(-42));
-console.log(numbers(42));
-console.log(numbers(0));
-console.log(numbers('ujl'));
+(numbers('2023 год'));
+(numbers(-42));
+(numbers(0));
+(numbers('ujl'));
+(numbers(''));
