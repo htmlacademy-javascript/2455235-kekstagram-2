d2ccddcd2af82fb05a8c85de928e86a011d3b711
TatianaSenatorova <108460915+TatianaSenatorova@users.noreply.github.com>
2024-11-14 12:04:42 +0300

    feat: полностью реализована функция закрытия и открытия формы

--- js/upload-photo.js
+++ js/upload-photo.js
@@ -1,14 +1,42 @@
 import { body } from './open-full-photo.js';
+import { isEscapeKey } from './utils.js';
 
 const imgUploadSection = document.querySelector('.img-upload');
 const imgUploadInput = imgUploadSection.querySelector('.img-upload__input');
 const imgUploadOverlay = imgUploadSection.querySelector('.img-upload__overlay');
-console.log(imgUploadInput, imgUploadOverlay);
+const imgUploadClose = imgUploadSection.querySelector('.img-upload__cancel');
+const imgHashtag = imgUploadSection.querySelector('.text__hashtags');
+const imgDescription = imgUploadSection.querySelector('.text__description');
 
-const openFormAddPhoto = () => {
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUploadForm();
+  }
+};
+
+const clearForm = () => {
+  imgUploadInput.value = '';
+  imgHashtag.value = '';
+  imgDescription.value = '';
+};
+
+
+function openUploadForm() {
   imgUploadOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
 }
 
-imgUploadInput.addEventListener('change', openFormAddPhoto);
+function closeUploadForm() {
+  imgUploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  clearForm();
+}
+
+imgUploadInput.addEventListener('change', openUploadForm);
+imgUploadClose.addEventListener('click', closeUploadForm);
+
 
